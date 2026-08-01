c31d77aa50d60514d6fdc3aaa7e0c6cf1d530685
Dave Clydesdale <47127754+ClydSpyd@users.noreply.github.com>
2026-08-01 10:34:14 +0200

    sidebar-removed

--- src/components/utility/ProtectedLayout.tsx
+++ src/components/utility/ProtectedLayout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 import ViewHeader from '../ui/ViewHeader';
-import Sidebar from '../ui/Sidebar';
 
 /**
  * Layout route for all authenticated pages. The auth guard lives on the route
@@ -12,7 +11,6 @@ const ProtectedLayout = () => {
     <div className="app-bg w-screen min-h-screen flex flex-col">
       <ViewHeader />
       <main className="container grow min-w-screen flex flex-1 overflow-hidden">
-        <Sidebar />
         <Outlet />
       </main>
     </div>
